2c9bca876b7d5be0dbb7102b4a05d7df7a458300
sndpoffcl <sandeep2official@gmail.com>
2024-03-21 23:18:13 +0530

    group controller

--- src/main/java/dev/sandeep/Splitwise/controller/GroupController.java
+++ src/main/java/dev/sandeep/Splitwise/controller/GroupController.java
@@ -1,9 +1,21 @@
 package dev.sandeep.Splitwise.controller;
 
+import dev.sandeep.Splitwise.entity.SettlementTransaction;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class GroupController {
 
-
+    @GetMapping("/settleup/{groupId}")
+    public ResponseEntity settleUp(@PathVariable("groupId") int groupId){
+        List<SettlementTransaction> settlementTransactionList = new ArrayList<>();
+        return ResponseEntity.ok(settlementTransactionList);
+    }
 }
